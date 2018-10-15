cca96f0a8ac855ff97d4704a76b39827e55cf3fc
Alex Shakun <alx.safari@gmail.com>
2018-10-16 00:28:02 +0300

    add 1.1 1.4 and 1.5

---
+++ src/chapter1/1.4/solution.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
---
+++ src/chapter1/1.5/solution.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(define (p) (p))
+
+(define (test x y)
+   (if (= x 0)
+       0
+       y))
+
+(test 0 (p))       
